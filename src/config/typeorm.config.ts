6e5279c5e49b66179a7535c07420655c795189da
jaime <jrossetes@uninorte.edu.co>
2021-04-17 20:42:42 -0400

    fix: server connects to db instance

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: 'db-postgres',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    database: 'tyba',
+    database: 'postgres',
     autoLoadEntities: true,
     synchronize: true
 }
